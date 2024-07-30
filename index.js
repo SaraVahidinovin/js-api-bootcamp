24be3b7dafa0f6d9794ae3c15abdeec597c974d0
unknown <sa.vahidinovin@gmail.com>
2024-07-30 11:16:12 +0200

    Render in DOM

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 async function fetchAndRenderPokemons() {
     try {
-        const response = await fetch('https://santosnr6.github.io/Data/pokemons.json');
-        const pokemons = await response.json();
+        const response = await fetch('https://santosnr6.github.io/Data/pokemons.json'); //fetch the data
+        const pokemons = await response.json(); //parse the JSON data
         console.log(pokemons);
 
-        pokemons.forEach(pokemon => console.log(pokemon));
+        pokemons.forEach(pokemon => console.log(pokemon)); // Log each Pokemon object to the console
+
+        pokemons.forEach(pokemon => {
+            document.body.innerHTML += `${pokemon.name}<br>`;
+        }); // Render the name of each Pokemon in the DOM
+
 
     } catch {
         console.log(error);
